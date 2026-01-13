b0ee7a5de3cb532b2456fef66eab31da94c66503
yeaminstudent5598 <yeaminstudent5598@gmail.com>
2026-01-13 18:22:59 +0600

    Update: src/modules/genre/genre.service.ts

--- src/modules/genre/genre.service.ts
+++ src/modules/genre/genre.service.ts
@@ -1,12 +1,41 @@
 import { Genre } from './genre.model';
 import { IGenre } from './genre.interface';
+import { Book } from '../book/book.model';
 
 const createGenreInDB = async (payload: IGenre) => {
   return await Genre.create(payload);
 };
 
 const getAllGenresFromDB = async () => {
-  return await Genre.find({ isDeleted: false });
+  const genres = await Genre.find({ isDeleted: false }).lean();
+
+  const genresWithCount = await Promise.all(
+    genres.map(async (genre) => {
+      const count = await Book.countDocuments({ 
+        genre: genre._id, 
+        isDeleted: false 
+      });
+      return {
+        ...genre,
+        bookCount: count 
+      };
+    })
+  );
+
+  return genresWithCount;
 };
 
-export const GenreService = { createGenreInDB, getAllGenresFromDB };
+const updateGenreInDB = async (id: string, payload: Partial<IGenre>) => {
+  return await Genre.findByIdAndUpdate(id, payload, { new: true });
+};
+
+const deleteGenreFromDB = async (id: string) => {
+  return await Genre.findByIdAndUpdate(id, { isDeleted: true }, { new: true });
+};
+
+export const GenreService = { 
+  createGenreInDB, 
+  getAllGenresFromDB,
+  updateGenreInDB,
+  deleteGenreFromDB
+};
