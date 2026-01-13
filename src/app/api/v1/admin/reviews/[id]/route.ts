ee27c23d9365b02c137aaf60160cbfc397af6e76
yeaminstudent5598 <yeaminstudent5598@gmail.com>
2026-01-13 23:21:58 +0600

    Update: src/app/api/v1/admin/reviews/[id]/route.ts

--- src/app/api/v1/admin/reviews/[id]/route.ts
+++ src/app/api/v1/admin/reviews/[id]/route.ts
@@ -1,11 +1,17 @@
 import { ReviewController } from '@/modules/review/review.controller';
 
-export async function PATCH(req: Request, { params }: { params: Promise<{ id: string }> }) {
-  const { id } = await params;
-  return ReviewController.updateReviewStatus(req, id);
+export async function PATCH(
+  req: Request, 
+  { params }: { params: Promise<{ id: string }> }
+) {
+  const { id } = await params; 
+  return ReviewController.approveReview(req, id);
 }
 
-export async function DELETE(req: Request, { params }: { params: Promise<{ id: string }> }) {
+export async function DELETE(
+  req: Request, 
+  { params }: { params: Promise<{ id: string }> }
+) {
   const { id } = await params;
   return ReviewController.deleteReview(id);
 }
