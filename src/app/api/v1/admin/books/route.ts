93275b9f9c3152fd01173db9d6c6a239325b0fec
yeaminstudent5598 <yeaminstudent5598@gmail.com>
2026-01-13 18:22:50 +0600

    Update: src/app/api/v1/admin/books/route.ts

---
+++ src/app/api/v1/admin/books/route.ts
@@ -0,0 +1,9 @@
+import { BookController } from "@/modules/book/book.controller";
+
+export async function POST(req: Request) {
+  return BookController.createBook(req);
+}
+
+export async function GET(req: Request) {
+  return BookController.getAllBooks(req);
+}
