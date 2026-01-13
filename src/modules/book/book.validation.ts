eec30012698f95fb03ba765415cabe2561772542
yeaminstudent5598 <yeaminstudent5598@gmail.com>
2026-01-13 18:22:57 +0600

    Update: src/modules/book/book.validation.ts

--- src/modules/book/book.validation.ts
+++ src/modules/book/book.validation.ts
@@ -4,8 +4,11 @@ const createBookSchema = z.object({
   title: z.string().min(1, 'Title is required'),
   author: z.string().min(1, 'Author is required'),
   genre: z.string().min(1, 'Genre ID is required'),
-  description: z.string().optional(),
-  coverImage: z.string().url('Invalid image URL').optional(),
+  description: z.string().min(10, 'Description must be at least 10 chars'),
+  pages: z.coerce.number().min(1, 'Pages must be at least 1'),
+  pubYear: z.coerce.number().min(1000, 'Invalid publication year'),
 });
 
-export const BookValidation = { createBookSchema };
+const updateBookSchema = createBookSchema.partial();
+
+export const BookValidation = { createBookSchema, updateBookSchema };
