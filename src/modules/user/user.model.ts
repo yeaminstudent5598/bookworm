a7d4638f0b2084311339fe36b2528d765c16cac4
yeaminstudent5598 <yeaminstudent5598@gmail.com>
2026-01-13 18:23:02 +0600

    Update: src/modules/user/user.model.ts

--- src/modules/user/user.model.ts
+++ src/modules/user/user.model.ts
@@ -6,7 +6,8 @@ const userSchema = new Schema<IUser>(
   {
     name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
-    password: { type: String, required: true, select: 0 },
+    password: { type: String, required: true, select: false }, // Security fix
+    photo: { type: String, required: true },
     role: { type: String, enum: ['user', 'admin'], default: 'user' },
     isDeleted: { type: Boolean, default: false },
     preferences: [{ type: Schema.Types.ObjectId, ref: 'Genre' }],
@@ -14,12 +15,11 @@ const userSchema = new Schema<IUser>(
   { timestamps: true }
 );
 
-// Password hashing before saving
 userSchema.pre('save', async function (next) {
   if (this.isModified('password')) {
     this.password = await bcrypt.hash(this.password!, 12);
   }
-//   next();
+  // next();
 });
 
 export const User = models.User || model<IUser>('User', userSchema);
