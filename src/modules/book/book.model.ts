afef50c4e01d48a04ac6e444103b230b9a02e911
yeaminstudent5598 <yeaminstudent5598@gmail.com>
2026-01-13 18:22:56 +0600

    Update: src/modules/book/book.model.ts

--- src/modules/book/book.model.ts
+++ src/modules/book/book.model.ts
@@ -3,11 +3,13 @@ import { IBook } from './book.interface';
 
 const bookSchema = new Schema<IBook>(
   {
-    title: { type: String, required: true },
-    author: { type: String, required: true },
+    title: { type: String, required: true, trim: true },
+    author: { type: String, required: true, trim: true },
     genre: { type: Schema.Types.ObjectId, ref: 'Genre', required: true },
-    description: { type: String },
-    coverImage: { type: String }, // URL from Cloudinary
+    description: { type: String, required: true },
+    coverImage: { type: String, required: true },
+    pages: { type: Number, required: true },
+    pubYear: { type: Number, required: true },
     averageRating: { type: Number, default: 0 },
     totalReviews: { type: Number, default: 0 },
     isDeleted: { type: Boolean, default: false },
