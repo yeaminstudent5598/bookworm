49192eb9552720c67bdc3de8e37c65f90f61953e
yeaminstudent5598 <yeaminstudent5598@gmail.com>
2026-01-13 18:22:52 +0600

    Update: src/app/api/v1/books/route.ts

--- src/app/api/v1/books/route.ts
+++ src/app/api/v1/books/route.ts
@@ -1,12 +1,9 @@
 import { BookController } from '@/modules/book/book.controller';
-import dbConnect from '@/lib/dbConnect';
 
 export async function POST(req: Request) {
-  await dbConnect();
   return BookController.createBook(req);
 }
 
 export async function GET(req: Request) {
-  await dbConnect();
   return BookController.getAllBooks(req);
 }
