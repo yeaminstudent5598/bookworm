964809983fa8bb14f015c1704c2efdd788486078
yeaminstudent5598 <yeaminstudent5598@gmail.com>
2026-01-13 18:22:50 +0600

    Update: src/app/api/v1/admin/books/[id]/route.ts

---
+++ src/app/api/v1/admin/books/[id]/route.ts
@@ -0,0 +1,25 @@
+import { BookController } from "@/modules/book/book.controller";
+
+export async function GET(
+  req: Request, 
+  { params }: { params: Promise<{ id: string }> }
+) {
+  const { id } = await params;
+  return BookController.getSingleBook(id);
+}
+
+export async function PATCH(
+  req: Request, 
+  { params }: { params: Promise<{ id: string }> }
+) {
+  const { id } = await params;
+  return BookController.updateBook(req, id);
+}
+
+export async function DELETE(
+  req: Request, 
+  { params }: { params: Promise<{ id: string }> }
+) {
+  const { id } = await params;
+  return BookController.deleteBook(id);
+}
