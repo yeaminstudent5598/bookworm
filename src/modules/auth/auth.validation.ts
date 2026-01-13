65d66a62c67ec5af002e5ad453050ff5705def68
yeaminstudent5598 <yeaminstudent5598@gmail.com>
2026-01-13 18:22:56 +0600

    Update: src/modules/auth/auth.validation.ts

--- src/modules/auth/auth.validation.ts
+++ src/modules/auth/auth.validation.ts
@@ -1,8 +1,14 @@
 import { z } from 'zod';
 
+const registerValidationSchema = z.object({
+  name: z.string().min(1, 'Name is required'),
+  email: z.string().email('Invalid email'),
+  password: z.string().min(6, 'Password must be 6+ chars'),
+});
+
 const loginValidationSchema = z.object({
   email: z.string().email('Invalid email'),
   password: z.string().min(1, 'Password is required'),
 });
 
-export const AuthValidation = { loginValidationSchema };
+export const AuthValidation = { registerValidationSchema, loginValidationSchema };
