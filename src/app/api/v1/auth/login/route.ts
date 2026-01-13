2dd46fda4713d6c0367001f85f1c2886e23d8498
yeaminstudent5598 <yeaminstudent5598@gmail.com>
2026-01-13 18:22:51 +0600

    Update: src/app/api/v1/auth/login/route.ts

--- src/app/api/v1/auth/login/route.ts
+++ src/app/api/v1/auth/login/route.ts
@@ -1,7 +1,9 @@
-import dbConnect from '@/lib/dbConnect';
-import { AuthController } from '@/modules/auth/auth.controlle';
+// ✅ CLEAN - src/app/api/v1/auth/login/route.ts
 
-export async function POST(req: Request) {
-  await dbConnect();
+import { AuthController } from "@/modules/auth/auth.controlle";
+import { NextRequest, NextResponse } from "next/server";
+
+// POST request handle
+export async function POST(req: NextRequest) {
   return AuthController.login(req);
 }
